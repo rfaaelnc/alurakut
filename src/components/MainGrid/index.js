d982129756be5a3327ac9072e6ad3afbd82934a7
rfaaelnc <68340580+rfaaelnc@users.noreply.github.com>
2021-07-19 03:03:42 -0300

    update v0.3

--- src/components/MainGrid/index.js
+++ src/components/MainGrid/index.js
@@ -21,6 +21,46 @@ const MainGrid = styled.main`
     "profileArea welcomeArea profileRelationsArea";
     grid-template-columns:160px 1fr 312px;
   }
+
+  .list {
+
+    li {
+    display:flex;
+    flex-wrap:wrap;
+    align-items:center;
+    background-color:#F1F9FE;
+    padding:15px;
+
+    &:first-child {
+      border-top-left-radius:10px;
+      border-top-right-radius: 10px;
+    }
+
+    &:nth-child(odd) {
+      background-color:#D9E6F6;
+    }
+
+    h3 {
+      padding-left:15px;
+    }
+
+      figure {
+        max-width:92px;
+        border-radius:100%;
+        max-width: 92px;
+        border-radius: 100%;
+        overflow: hidden;
+        border: 1px solid #f2f2f2;
+        background-color:#f2f2f2;
+        
+        img {
+          object-fit: cover;
+          width: 92px;
+          height: 92px;
+        }
+      }
+    }
+  }
 `;
 
 export default MainGrid;
